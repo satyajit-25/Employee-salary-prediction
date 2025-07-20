85ab44b309a4eb2c62537c958efbc1686ccd2feb
Satyajit Patra <satyajit.patra203@gmail.com>
2025-07-21 00:02:01 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("Cleaned_Salary_Data.csv")
+
+def run():
+
+    
+    st.title("Employee Salary Prediction 📈💰🔗")
+
+    try:
+        df = load_data()
+
+        # Display a sample
+        st.dataframe(df.head(200), use_container_width=True)
+
+        st.write("***")
+        st.subheader("Data Summary Overview 🧐")
+
+        num_data = df.select_dtypes(include="number")
+        cat_data = df.select_dtypes(include="object")
+
+        if not num_data.empty:
+            st.subheader("🔢 Numerical Data Summary")
+            st.table(num_data.describe().T)
+
+        if not cat_data.empty:
+            st.subheader("🔤 Categorical Data Summary")
+            st.table(cat_data.describe().T)
+
+    except FileNotFoundError:
+        st.error("❌ Dataset file not found. Please make sure it's uploaded or placed correctly.")
